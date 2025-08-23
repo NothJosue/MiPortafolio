766daef60041382d0efc3bdad94adbc25d825d7f
NothJosue <thedarkm04@gmail.com>
2025-08-23 00:55:30 -0500

    Agregar funcionalidad de la barra lateral nav-menu

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const navMenu = document.querySelector(".nav-menu");
 navToggle.addEventListener("click", () =>{
     navMenu.classList.toggle("nav-menu_visible");
 });
+
+const navMenuLinks = document.querySelectorAll(".nav-menu-link");
+navMenuLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav-menu_visible");
+  });
+});
